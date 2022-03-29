bb810c74b4bc052f68bb2cef731a76cc09d007df
joycesaquino <joycesaquino@gmail.com>
2022-03-29 20:08:15 -0300

    Creating local stack configuration

---
+++ config/init-scripts/init-db.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+function configure(){
+    aws configure set aws_access_key_id default_access_key
+    aws configure set aws_secret_access_key default_secret_key
+    aws configure set region sa-east-1
+}
+
+function createTable() {
+    printf 'Creating order book table'
+    aws dynamodb --endpoint-url=http://localhost:4566 create-table --table-name order-book-operation \
+    --attribute-definitions \
+        AttributeName=id,AttributeType=S \
+        AttributeName=operationType,AttributeType=S \
+        AttributeName=operationStatus,AttributeType=S \
+    --key-schema \
+        AttributeName=operationType,KeyType=HASH \
+        AttributeName=id,KeyType=RANGE \
+    --global-secondary-indexes  \
+    "[
+            {
+                \"IndexName\": \"operationStatusAndOperationTypeIndex\",
+                \"KeySchema\": [
+                    {\"AttributeName\":\"operationStatus\",\"KeyType\":\"HASH\"},
+                    {\"AttributeName\":\"operationType\",\"KeyType\":\"RANGE\"}
+                ],
+                \"Projection\": {
+                    \"ProjectionType\":\"ALL\"
+                },
+                \"ProvisionedThroughput\": {
+                    \"ReadCapacityUnits\": 10,
+                    \"WriteCapacityUnits\": 5
+                }
+            }
+        ]" \
+    --stream-specification StreamEnabled=TRUE,StreamViewType=NEW_AND_OLD_IMAGES \
+    --provisioned-throughput ReadCapacityUnits=10,WriteCapacityUnits=5
+}
+
+configure
+createTable
+
+printf '\nListing all created tables'
+
+aws dynamodb list-tables
